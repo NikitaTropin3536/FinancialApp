a716939da12adea0fe78a7e3ec7a10bd390b6ed1
Nikita Tropin <tropinikita.06@mail.ru>
2025-06-30 18:37:53 +0300

    Removed unnecessary dependencies

--- app/src/main/java/com/atech/financialapp/component/nav/BottomBar.kt
+++ app/src/main/java/com/atech/financialapp/component/nav/BottomBar.kt
@@ -1,6 +1,5 @@
 package com.atech.financialapp.component.nav
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -10,7 +9,6 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
